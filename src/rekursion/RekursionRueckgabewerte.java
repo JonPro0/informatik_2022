3ee914b1d01b8a79f609d3904613a0688dcfd04f
JonPro00 <zellerhoff05@gmail.com>
2022-02-02 10:17:28 +0100

    add kaninchensex

---
+++ src/rekursion/RekursionRueckgabewerte.java
@@ -0,0 +1,49 @@
+package rekursion;
+
+
+public class RekursionRueckgabewerte {
+    private static int ergebnisJonas = 1;
+
+
+    public static int fakultaet1(int zahl){
+        int ergebnis = zahl;
+        for (int i = zahl - 1; i > 0; i--) {
+            ergebnis *= i;
+        }
+        return ergebnis;
+    }
+
+    public static int fakultaet2(int n){
+        int ergebnis = 1;
+        for (int i = 2; i <= n; i++) {
+            ergebnis *= i;
+        }
+        return ergebnis;
+    }
+
+    public static int fakultaetRekursiv(int zahl){
+        if(zahl == 0 || zahl == 1)return 1;
+        return zahl * fakultaetRekursiv(zahl-1);
+    }
+
+    //TODO: Formel: f(n) = f(n-1) + f(n-2)
+
+    public static int kaninchensex(int n){
+        return (n < 2) ? 1 : kaninchensex(n-1) + kaninchensex(n-2);
+    }
+
+
+
+    public static void main(String[] args) {
+        //System.out.println(fakultaetRekursiv(0));
+        long i = System.currentTimeMillis();
+        System.out.println(fakultaet1(15));
+        long ii = System.currentTimeMillis();
+        System.out.println("Zeit: " + (ii - i));
+
+        long a = System.currentTimeMillis();
+        System.out.println(fakultaet2(15));
+        double aa = System.currentTimeMillis();
+        System.out.println("Zeit: " + (aa - a));
+    }
+}
