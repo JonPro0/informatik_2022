99a2b7eafddf3554a68d3b5b0187cde11c62d0af
JonPro00 <zellerhoff05@gmail.com>
2022-03-08 12:04:03 +0100

    feat: Ackermann class with new function called doAckermann

--- src/rekursion/Ackermann.java
+++ src/rekursion/Ackermann.java
@@ -1,4 +1,16 @@
 package rekursion;
 
 public class Ackermann {
+    private static long doAckermann(long n, long m){
+        if(n == 0){
+            return m + 1;
+        } else if (n != 0 && m == 0){
+            return doAckermann(n -1, 1);
+        } else {
+            return doAckermann(n-1, doAckermann(n, m-1));
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(doAckermann(1, 0));
+    }
 }
