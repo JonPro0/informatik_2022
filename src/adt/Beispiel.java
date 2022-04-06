d543a4dcf5c08ae5e5d30cec5a42094afe21424e
JonPro00 <zellerhoff05@gmail.com>
2022-04-06 10:22:02 +0200

    Queue

---
+++ src/adt/Beispiel.java
@@ -0,0 +1,12 @@
+package adt;
+
+public class Beispiel {
+    int lz;
+    String lf;
+    public static void main(String[] args) {
+        Beispiel b = new Beispiel();
+        b.lf = "rot";
+        b.lz = 5;
+        System.out.println(b);
+    }
+}
---
+++ src/adt/Queue.java
@@ -0,0 +1,31 @@
+package adt;
+
+public class Queue {
+    Item first;
+
+    public Queue() {
+        first = null;
+    }
+
+    public void add(int data){
+        Item tmp = new Item(data);
+        if (first == null){
+            first = tmp;
+        } else {
+            Item runner = first;
+            while (runner.next != null){
+                runner = runner.next;
+            }
+            runner = tmp;
+        }
+    }
+
+}
+
+class Item {
+    int data;
+    Item next;
+    public Item(int data){
+        this.data = data;
+    }
+}
