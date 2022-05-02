fab0aaac315515653058dee24c5fac0ab702a243
JonPro0 <zellerhoff05@gmail.com>
2022-05-02 15:53:39 +0200

    feat: modified Eisdiele

--- src/eisdiele_projekt/Eisdiele.java
+++ src/eisdiele_projekt/Eisdiele.java
@@ -1,33 +1,57 @@
 package eisdiele_projekt;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Eisdiele extends JFrame{
     private JPanel pMain;
     private JPanel pKunde;
     private JPanel pEisdieler;
-    private JButton bSorte1;
-    private JButton bSorte2;
-    private JButton bSorte3;
-    private JButton bSorte4;
-    private JButton bSorte5;
-    private JButton bSorte6;
-    private JButton bSorte7;
     private JTextArea tName;
     private JLabel pInfo;
     private JLabel pInfo2;
     private JLabel pInfo3;
+    private JTextArea tEis;
+    private JLabel lName;
+    private JLabel lEissorte;
+    private JButton bBestellen;
+    private String kunde;
+    private int kundenCounter = 1;
 
     public Eisdiele (){
         super("ZellEisdiele");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        bBestellen.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!proofName()){
+                    kunde = "Kunde" + kundenCounter;
+                    kundenCounter++;
+                } else {
+                    kunde = tName.getText();
+                }
+                if (!proofIce()){
+                    JOptionPane.showMessageDialog(null, "Du musst eine Eissorte eintagen!!");
+                }
+            }
 
+
+        });
 
 
         add(pMain);
         setSize(800, 600);
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private boolean proofIce() {
+        return tEis.getText() != null;
+    }
+
+    private boolean proofName() {
+        return tName.getText() == null;
     }
 }
